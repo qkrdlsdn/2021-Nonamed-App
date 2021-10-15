207d27bbc0a5a1ac2f072bcc0c92e071061170a7
qkrdlsdn <parkinwoo0626@gmail.com>
2021-10-15 20:24:24 +0900

    nonamed project v2

--- app/src/main/java/com/example/a2021_nonamedproject_v1/MainActivity.java
+++ app/src/main/java/com/example/a2021_nonamedproject_v1/MainActivity.java
@@ -1,17 +1,214 @@
 package com.example.a2021_nonamedproject_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter btAdapter;
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    private TextView textStatus;
+    private Button btnParied, btnSearch, btnSend;
+    private ListView listView;
+
+    private Set<BluetoothDevice> pairedDevices;
+    private ArrayAdapter<String> btArrayAdapter;
+    private ArrayList<String> deviceAddressArray;
+
+    private BluetoothSocket btSocket;
+    private ConnectedThread connectedThread;
+
+    private boolean isCtrlFlag;
+    private boolean isConnected;
+    private View bluetooth, control;
+
+    private Button disconnect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        isCtrlFlag = false;
+        initWidget();
+        changeMod(isCtrlFlag);
+
+        // Get permission
+        String[] permission_list = {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+        };
+
+        ActivityCompat.requestPermissions(MainActivity.this, permission_list,  1);
+
+        // Enable bluetooth
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        // show paired devices
+        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        deviceAddressArray = new ArrayList<>();
+        listView.setAdapter(btArrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+
+                textStatus.setText("try...");
+
+                final String name = btArrayAdapter.getItem(position); // get name
+                final String address = deviceAddressArray.get(position); // get address
+                boolean flag = true;
+
+                BluetoothDevice device = btAdapter.getRemoteDevice(address);
+
+                // create & connect socket
+                try {
+                    btSocket.connect();
+                } catch (IOException e) {
+                    flag = false;
+                    textStatus.setText("connection failed!");
+                    e.printStackTrace();
+                }
+
+                if(flag){
+                    textStatus.setText("connected to "+name);
+                    connectedThread = new ConnectedThread(btSocket);
+                    connectedThread.start();
+
+                    isCtrlFlag = true;
+                    changeMod(isCtrlFlag);
+                }
+            }
+        });
+    }
+
+    public void initWidget() {
+        textStatus = (TextView) findViewById(R.id.text_status);
+        btnParied = (Button) findViewById(R.id.btn_paired);
+        btnSearch = (Button) findViewById(R.id.btn_search);
+        listView = (ListView) findViewById(R.id.listview);
+
+        bluetooth = (View) findViewById(R.id.bluetooth);
+        control = (View) findViewById(R.id.control);
+        disconnect = (Button) findViewById(R.id.btn_disconnect);
+    }
+
+    public void changeMod(boolean isCtrlFlag) {
+        if(isCtrlFlag) {
+            bluetooth.setVisibility(View.GONE);
+            control.setVisibility(View.VISIBLE);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+        else {
+            bluetooth.setVisibility(View.VISIBLE);
+            control.setVisibility(View.GONE);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
+    }
+
+    public void onClickButtonPaired(View view){
+        btArrayAdapter.clear();
+        if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
+        pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+            }
+        }
+    }
+
+    public void onClickButtonSearch(View view){
+        // Check if the device is already discovering
+        if(btAdapter.isDiscovering()){
+            btAdapter.cancelDiscovery();
+        } else {
+            if (btAdapter.isEnabled()) {
+                btAdapter.startDiscovery();
+                btArrayAdapter.clear();
+                if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
+                    deviceAddressArray.clear();
+                }
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(receiver, filter);
+            } else {
+                Toast.makeText(getApplicationContext(), "bluetooth not on", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                btArrayAdapter.add(deviceName);
+                deviceAddressArray.add(deviceHardwareAddress);
+                btArrayAdapter.notifyDataSetChanged();
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(receiver);
+    }
+
+    public void onClickFront(View view) {
+        if(connectedThread!=null){ connectedThread.write("f"); }
+    }
+
+    public void onClickBack(View view) {
+        if(connectedThread!=null){ connectedThread.write("b"); }
+    }
+
+    public void onClickLeft(View view) {
+        if(connectedThread!=null){ connectedThread.write("l"); }
+    }
+
+    public void onClickRight(View view) {
+        if(connectedThread!=null){ connectedThread.write("r"); }
+    }
+
+    public void onClickDisconnect(View view) {
+        onBackPressed();
     }
 }
